8fc5bfb627bc933676c1489f2cfaf75589328b42
Bruno Marques <brunolmarques@gmail.com>
2023-01-28 11:01:28 -0300

    adjusted if condition on postgres script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -30,15 +30,27 @@ DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
 # Launch postgres using Podman
-podman run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d docker.io/library/postgres \
-    postgres -N 1000
-    # ^ Increased maximum number of connections for testing purposes
-
+if [[ $1 ]]
+then
+    # if a Postgres container is running, print instructions to kill it and exit
+    RUNNING_POSTGRES_CONTAINER=$(podman ps --filter 'name=postgres' --format '{{.ID}}')
+    if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+    echo >&2 "There is a Postgres container already running, kill it with"
+    echo >&2 "    podman kill ${RUNNING_POSTGRES_CONTAINER}"
+    exit 1
+    fi
+    # Launch postgres using Podman
+    podman run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -p "${DB_PORT}":5432 \
+        -d \
+        --name "postgres_$(date '+%s')" \
+        docker.io/library/postgres \
+        postgres -N 1000
+        # ^ Increased maximum number of connections for testing purposes
+fi
 
 # Set password fot psql
 export PGPASSWORD="${DB_PASSWORD}"
